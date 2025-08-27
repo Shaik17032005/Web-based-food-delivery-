f09682448a34d043ca3085d5f79b6a1ea14b95ca
= <=>
2025-08-27 13:15:07 +0530

    chnages

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const backToTop = document.getElementById("backToTop");
+
+window.onscroll = function() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    backToTop.style.display = "block";
+  } else {
+    backToTop.style.display = "none";
+  }
+};
+
+backToTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
